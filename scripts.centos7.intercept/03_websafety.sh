def8bb8be67ad1339c548f9454f20a91e7bfa646
Rafael Akchurin <rafael.akchurin@diladele.com>
2017-02-27 18:53:09 +0100

    added scripts for all platforms for new 5.0 build

---
+++ scripts.centos7.intercept/03_websafety.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# all packages are installed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# default arc
+MAJOR="5.0.0"
+MINOR="0D2A"
+
+# get latest build
+curl -O http://packages.diladele.com/websafety/$MAJOR.$MINOR/amd64/release/centos7/websafety-${MAJOR}-${MINOR}.x86_64.rpm
+
+# install it
+yum -y --nogpgcheck localinstall websafety-${MAJOR}-${MINOR}.x86_64.rpm
+  
+# package installed everything needed for apache, so just restart
+systemctl restart httpd.service
---
+++ scripts.debian8/06_websafety.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# all packages are installed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# default arc
+MAJOR="5.0.0"
+MINOR="0D2A"
+ARCH="amd64"
+
+# get latest build
+cat /proc/cpuinfo | grep -m 1 ARMv7 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	ARCH="armhf"
+fi
+
+wget http://packages.diladele.com/websafety/$MAJOR.$MINOR/$ARCH/release/debian8/websafety-$MAJOR.${MINOR}_$ARCH.deb
+
+# install it
+dpkg --install websafety-$MAJOR.${MINOR}_$ARCH.deb
+
+# relabel log folder
+chown -R websafety:websafety /opt/websafety
