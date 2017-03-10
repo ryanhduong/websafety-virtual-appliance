4f5c6f75bab56af9a4e3fb30beb54deb913fbd3d
Rafael Akchurin <rafael.akchurin@diladele.com>
2017-03-10 17:24:17 +0100

    new 5.0 build

--- scripts.centos7/03_websafety.sh
+++ scripts.centos7/03_websafety.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="5.0.0"
-MINOR="0D2A"
+MINOR="D465"
 
 # get latest build
 curl -O http://packages.diladele.com/websafety/$MAJOR.$MINOR/amd64/release/centos7/websafety-${MAJOR}-${MINOR}.x86_64.rpm
