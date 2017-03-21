1b7c4532beb2914cb798db20e3f69a261956d657
Rafael Akchurin <rafael.akchurin@diladele.com>
2017-03-21 22:07:30 +0100

    next build

--- scripts.freebsd10/04_websafety.sh
+++ scripts.freebsd10/04_websafety.sh
@@ -4,7 +4,7 @@
 ARCH=`uname -m`
 
 DDWS_VERSION=5.0.0
-DDWS_BUILD=8FE5
+DDWS_BUILD=1DC9
 
 # get latest version of web safety
 fetch http://packages.diladele.com/websafety/$DDWS_VERSION.$DDWS_BUILD/$ARCH/release/freebsd10/websafety-$DDWS_VERSION-$ARCH.txz
--- scripts.ubuntu16/04_websafety.sh
+++ scripts.ubuntu16/04_websafety.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="5.0.0"
-MINOR="8FE5"
+MINOR="1DC9"
 ARCH="amd64"
 
 # download
