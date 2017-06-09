d95881b85b6371fffa4b747d9d0089a63c9f8095
Rafael Akchurin <rafael.akchurin@diladele.com>
2017-06-09 09:08:32 +0200

    fixed install errors

--- scripts.pfsense/prerequisites.sh
+++ scripts.pfsense/prerequisites.sh
@@ -9,12 +9,25 @@ pkg add $REPOURL/db5-5.3.28_3.txz
 pkg add $REPOURL/apr-1.5.2.1.5.4.txz
 pkg add $REPOURL/apache24-2.4.18.txz
 pkg add $REPOURL/ap24-mod_wsgi4-4.4.21.txz
+pkg add $REPOURL/sudo-1.8.15.txz
 
 # install django and sqlite modules for python
 pkg add $REPOURL/py27-setuptools27-19.2.txz
 pkg add $REPOURL/py27-sqlite3-2.7.11_7.txz
 pkg add $REPOURL/py27-django18-1.8.10.txz
 pkg add $REPOURL/py27-pytz-2015.7,1.txz
+pkg add $REPOURL/python2-2_3.txz
+pkg add $REPOURL/py27-pyasn1-0.1.9.txz
+pkg add $REPOURL/py27-pyasn1-modules-0.0.8_1.txz
+pkg add $REPOURL/py27-ldap-2.4.22.txz
+pkg add $REPOURL/py27-six-1.9.0.txz
+pkg add $REPOURL/py27-enum34-1.0.4.txz
+pkg add $REPOURL/py27-pycparser-2.10.txz
+pkg add $REPOURL/py27-cffi-1.2.1.txz
+pkg add $REPOURL/py27-idna-2.0.txz
+pkg add $REPOURL/py27-ipaddress-1.0.14.txz
+pkg add $REPOURL/py27-cryptography-1.0.2_4.txz
+pkg add $REPOURL/py27-openssl-0.15.1.txz
 
 # in order to correctly start up apache at boot time init script needs to be renamed
 cp /usr/local/etc/rc.d/apache24 /usr/local/etc/rc.d/apache24.sh
