85858cf97550a574393bd7094d7854ef1b4f4fdb
Rafael Akchurin <rafael.akchurin@diladele.lan>
2017-10-08 17:10:42 +0200

    new 5.3 build

--- scripts.pfsense/websafety.sh
+++ scripts.pfsense/websafety.sh
@@ -23,7 +23,7 @@ endif
 # how to check user websafety is in websafety group???
 
 # get latest version of diladele icap server
-fetch http://packages.diladele.com/websafety/5.3.0.3323/amd64/release/freebsd10/websafety-5.3.0-amd64.txz
+fetch http://packages.diladele.com/websafety/5.3.0.7472/amd64/release/freebsd10/websafety-5.3.0-amd64.txz
 
 # and install it
 pkg install -y websafety-5.3.0-amd64.txz
