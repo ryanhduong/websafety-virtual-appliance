688aaa13d0aef48904f3e99fa8a585179579bb34
Rafael Akchurin <rafael.akchurin@diladele.lan>
2017-10-19 12:35:58 +0200

    new build of 5.3

--- scripts.freebsd11/04_websafety.sh
+++ scripts.freebsd11/04_websafety.sh
@@ -4,7 +4,7 @@
 ARCH=`uname -m`
 
 DDWS_VERSION=5.3.0
-DDWS_BUILD=3651
+DDWS_BUILD=BA07
 
 # get latest version of web safety
 fetch http://packages.diladele.com/websafety/$DDWS_VERSION.$DDWS_BUILD/$ARCH/release/freebsd11/websafety-$DDWS_VERSION-$ARCH.txz
--- scripts.ubuntu16.intercept/04_websafety.sh
+++ scripts.ubuntu16.intercept/04_websafety.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="5.3.0"
-MINOR="3651"
+MINOR="BA07"
 ARCH="amd64"
 
 # download
