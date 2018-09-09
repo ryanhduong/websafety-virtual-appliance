09e4a1c4d149ad6a756947078ba0559868ca2706
Rafael Akchurin <rafael.akchurin@diladele.com>
2018-09-09 17:21:29 +0200

    switching websafety to squid 4

--- scripts.debian9/06_websafety.sh
+++ scripts.debian9/06_websafety.sh
@@ -22,5 +22,23 @@ wget http://packages.diladele.com/websafety/$MAJOR.$MINOR/$ARCH/release/debian9/
 # install it
 dpkg --install websafety-$MAJOR.${MINOR}_$ARCH.deb
 
-# relabel log folder
+# set new license if present
+if [ -f license.pem ]; then
+    sudo -u websafety cp license.pem /opt/websafety/etc
+fi
+
+# patch one file switching web safety to squid 4
+patch /opt/websafety/var/console/_domain/squid/binary_squid.py < binary_squid.py.patch
+
+# generate the configuration files
+sudo -u websafety python /opt/websafety/var/console/generate.py
+
+# relabel folder
 chown -R websafety:websafety /opt/websafety
+
+echo "WEB SAFETY INSTALLED"
+echo "WEB SAFETY INSTALLED"
+echo "WEB SAFETY INSTALLED --- License is till ---"
+cat /opt/websafety/etc/license.pem | grep "Not After"
+echo "WEB SAFETY INSTALLED"
+echo "WEB SAFETY INSTALLED"
