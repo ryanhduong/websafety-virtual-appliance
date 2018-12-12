1fd3e916c5cc5121677cb891c64885eba4f7dc71
Rafael Akchurin <rafael.akchurin@diladele.com>
2018-12-12 06:17:55 +0100

    increased FD count to 65K

--- scripts.debian9/05_install.sh
+++ scripts.debian9/05_install.sh
@@ -30,3 +30,17 @@ popd
 
 # put the squid on hold to prevent updating
 apt-mark hold squid squidclient squid-common squid-langpack
+
+# change the number of default file descriptors
+OVERRIDE_DIR=/etc/systemd/system/squid.service.d
+OVERRIDE_CNF=$OVERRIDE_DIR/override.conf
+
+mkdir -p $OVERRIDE_DIR
+
+# generate the override file
+rm $OVERRIDE_CNF
+echo "[Service]"         >> $OVERRIDE_CNF
+echo "LimitNOFILE=65535" >> $OVERRIDE_CNF
+
+# and reload the systemd
+systemctl daemon-reload
