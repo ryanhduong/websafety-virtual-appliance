e21e06787ca631d7855c94a066ba32533e5fa73a
Rafael Akchurin <rafael.akchurin@diladele.com>
2018-12-12 11:30:03 +0100

    added initial config generation

--- scripts.freebsd11/04_websafety.sh
+++ scripts.freebsd11/04_websafety.sh
@@ -3,7 +3,7 @@
 # setup some configuration variables
 ARCH=`uname -m`
 DDWS_VERSION=7.0.0
-DDWS_BUILD=8408
+DDWS_BUILD=3D2A
 
 # get latest version of web safety
 fetch http://packages.diladele.com/websafety/$DDWS_VERSION.$DDWS_BUILD/$ARCH/release/freebsd11/websafety-$DDWS_VERSION-$ARCH.txz
@@ -40,3 +40,9 @@ grep -e '^\s*wsytgd_enable\s*=\s*\"YES\"\s*$' /etc/rc.conf
 if [ $? -ne 0 ]; then
     echo "wsytgd_enable=\"YES\"" >> /etc/rc.conf
 fi
+
+# generate the configuration files once
+sudo -u websafety python3 /opt/websafety/var/console/generate.py
+
+# relabel folder
+chown -R websafety:websafety /opt/websafety
